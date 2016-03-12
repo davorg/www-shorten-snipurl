cd11e43c317101faa597fd54fc259ebb606efd13
Chase Whitener <cwhitener@gmail.com>
2016-03-12 17:19:51 -0500

    Initial cleanup mostly complete

--- lib/WWW/Shorten/SnipURL.pm
+++ lib/WWW/Shorten/SnipURL.pm
@@ -1,4 +1,51 @@
-# $Id$
+package WWW::Shorten::SnipURL;
+
+use 5.006;
+use strict;
+use warnings;
+
+use Carp ();
+use URI;
+use HTTP::Request::Common 'POST';
+use Try::Tiny qw(try catch);
+
+use base qw( WWW::Shorten::generic Exporter );
+our @EXPORT  = qw(makeashorterlink makealongerlink);
+our $VERSION = '2.020';
+$VERSION = eval $VERSION;
+
+sub makeashorterlink {
+    my $url     = shift or Carp::croak('No URL passed to makeashorterlink');
+    my $ua      = __PACKAGE__->ua();
+    my $snipurl = 'http://snipurl.com/site/index';
+    my $req     = POST $snipurl, [url => $url,];
+    my $resp
+        = try { return $ua->request($req); } catch { warn $_; return undef };
+    return unless $resp;
+    return unless $resp->is_success;
+
+    if ($resp->content
+        =~ m|<input name="SNIPPED" class="snipped textsnipped" type="text" value="(http://snipurl.com/\w+)"|
+        )
+    {
+        return $1;
+    }
+    return;
+}
+
+sub makealongerlink {
+    my $code = shift
+        or Carp::croak('No SnipURL key / URL passed to makealongerlink');
+    my $ua = __PACKAGE__->ua();
+    $code = "http://snipurl.com/$code" unless ($code =~ m|^http://|i);
+
+    my $resp = try { return $ua->get($code); } catch { warn $_; return undef };
+    return unless $resp;
+    return unless $resp->is_redirect;
+    return $resp->header('Location');
+}
+
+1;
 
 =head1 NAME
 
@@ -20,22 +67,6 @@ A Perl interface to the web service SnipURL.com. SnipURL maintains a
 database of long URLs, each of which has a unique identifier or
 nickname. For more features, please visit http://snipurl.com/features
 
-=cut
-
-package WWW::Shorten::SnipURL;
-
-use 5.006;
-use strict;
-use warnings;
-
-use base qw( WWW::Shorten::generic Exporter );
-our @EXPORT = qw(makeashorterlink makealongerlink);
-our $VERSION = '2.01';
-
-use Carp;
-use URI;
-use HTTP::Request::Common 'POST';
-
 =head1 Functions
 
 =head2 makeashorterlink
@@ -44,30 +75,6 @@ The function C<makeashorterlink> will call the SnipURL web site passing it
 your long URL and will return the shorter SnipURL version. If used in a
 list context, then it will return both the Snip URL and the password.
 
-=cut
-
-sub makeashorterlink {
-  my $url = shift or croak 'No URL passed to makeashorterlink';
-  my $ua = __PACKAGE__->ua();
-
-  my $snipurl = 'http://snipurl.com/site/index';
-
-  my $req = POST $snipurl,
-    [
-     url => $url,
-    ];
-
-  my $resp = $ua->request($req);
-
-  return unless $resp->is_success;
-
-  if ($resp->content =~ m|<input name="SNIPPED" class="snipped textsnipped" type="text" value="(http://snipurl.com/\w+)"|) {
-    return $1;
-  }
-
-  return;
-}
-
 =head2 makealongerlink
 
 The function C<makealongerlink> does the reverse. C<makealongerlink>
@@ -75,27 +82,6 @@ will accept as an argument either the full Snip URL or just the
 SnipURL identifier.
 
 If anything goes wrong, then either function will return C<undef>.
-
-=cut
-
-sub makealongerlink {
-  my $code = shift
-    or croak 'No SnipURL key / URL passed to makealongerlink';
-  my $ua = __PACKAGE__->ua();
-
-  unless ($code =~ m|^http://|) {
-    $code = "http://snipurl.com/$code";
-  }
-
-  my $resp = $ua->get($code);
-  return unless $resp->is_redirect;
-
-  return $resp->header('Location');
-}
-
-1;
-
-__END__
 
 =head2 EXPORT
 
